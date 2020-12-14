4a6cfe4bb79b062995fcede991ec7e06ce92fe44
Jaroslav Smekal <smekja@seznam.cz>
2020-12-14 20:04:27 +0100

    added BeforeEach hook

--- src/test/java/MavenClassTest.java
+++ src/test/java/MavenClassTest.java
@@ -1,13 +1,18 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 
 class MavenClassTest {
+    MavenClass mavenClass;
 
+    @BeforeEach
+    void init() {
+        mavenClass = new MavenClass();
+    }
     @Test
     void testAdd() {
-        MavenClass mavenClass = new MavenClass();
         int expected = 6;
         int actual = mavenClass.add(3, 3);
         Assertions.assertEquals(expected, actual);
@@ -15,7 +20,6 @@ class MavenClassTest {
 
     @Test
     void testAdd2() {
-        MavenClass mavenClass = new MavenClass();
         boolean thrown = false;
         try {
             mavenClass.add(Integer.MAX_VALUE, Integer.MAX_VALUE);
@@ -28,7 +32,6 @@ class MavenClassTest {
     @Test
     // different way of testing for exceptions
     void testAdd3() {
-        MavenClass mavenClass = new MavenClass();
         Assertions.assertThrows(ArithmeticException.class, () -> mavenClass.add(Integer.MAX_VALUE, Integer.MAX_VALUE),
                 "Overflow should throw arith exception.");
     }
