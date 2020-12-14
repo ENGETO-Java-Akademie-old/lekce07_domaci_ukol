4f51c0e9707f3af942413f6f6403f6e5296a783c
Jaroslav Smekal <smekja@seznam.cz>
2020-12-14 20:34:45 +0100

    Adding annotations

--- src/test/java/MavenClassTest.java
+++ src/test/java/MavenClassTest.java
@@ -1,9 +1,12 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-
-
+// Notes
+// BeforeAll and AfterAll methods has to be static, because they are run before the instance of class is created
+// Instances are created for every @Test and terminated afterwards
+// Annotations: @DisplayName("Name of test") to give names to tests instead default method name
+// @Disabled - skips the test
+// @EnabledOnOs(OS.LINUX) - test runs only on specific OS ; @EnabledOnJRE(JRE.JAVA_11) specific JRE version
+// assumeTrue(value); Hey Junit, dont continue running the test if the assumption is not true (ex: server has to be online)
 class MavenClassTest {
     MavenClass mavenClass;
 
@@ -12,6 +15,7 @@ class MavenClassTest {
         mavenClass = new MavenClass();
     }
     @Test
+    @DisplayName("Testing add class with small values.")
     void testAdd() {
         int expected = 6;
         int actual = mavenClass.add(3, 3);
@@ -36,5 +40,11 @@ class MavenClassTest {
                 "Overflow should throw arith exception.");
     }
 
+    @Test
+    @Disabled
+    void testSkipped() {
+        Assertions.assertThrows(ArithmeticException.class, () -> mavenClass.add(Integer.MAX_VALUE, Integer.MAX_VALUE),
+                "Overflow should throw arith exception.");
+    }
 
 }
